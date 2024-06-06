dc22a3f4078c52f8c02b26b94e60599390f5a4a4
jonygiraldo <jrubianogirald@gmail.com>
2024-06-06 15:57:57 +0200

    Agregar exótico exóticooo yeah

---
+++ app/src/main/java/com/example/tfgapplibros/data/UsuarioRepository.kt
@@ -0,0 +1,106 @@
+package com.example.tfgapplibros.data
+
+import android.net.Uri
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.tasks.await
+
+class UsuarioRepository {
+    private val db = FirebaseFirestore.getInstance()
+    private val storage = FirebaseStorage.getInstance()
+
+    fun guardarUsuario(
+        usuario: Usuario,
+        fotoPerfil: Uri?,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        val idUsuario = db
+            .collection("usuarios")
+            .document().id
+        val imgRef = storage.reference.child("usuarios/$idUsuario.jpg")
+
+        if (fotoPerfil != null) {
+            imgRef.putFile(fotoPerfil)
+                .addOnSuccessListener {
+                    imgRef.downloadUrl.addOnSuccessListener {
+                        val usuarioActualizado = usuario.copy(fotoPerfil = it.toString(), idUsuario = idUsuario)
+                        guardarUsuarioDb(idUsuario, usuarioActualizado, onSuccess, onFailure)
+                    }.addOnFailureListener { onFailure(it) }
+                }.addOnFailureListener { onFailure(it) }
+        }
+    }
+
+    private fun guardarUsuarioDb(
+        idUsuario: String,
+        usuario: Usuario,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        db.collection("usuarios")
+            .document(idUsuario)
+            .set(usuario)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { onFailure(it) }
+    }
+
+    fun actualizarUsuario(
+        idUsuario: String,
+        usuario: Usuario,
+        fotoPerfil: Uri,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        val imgRef = storage.reference.child("usuarios/$idUsuario.jpg")
+        imgRef.delete()
+            .addOnSuccessListener {
+                //Esta uri esta mal
+                imgRef.putFile(fotoPerfil)
+                    .addOnSuccessListener {
+                        imgRef.downloadUrl.addOnSuccessListener {
+                            val usuarioActualizado =
+                                usuario.copy(fotoPerfil = it.toString(), idUsuario = idUsuario)
+                            guardarUsuarioDb(idUsuario, usuarioActualizado, onSuccess, onFailure)
+                        }.addOnFailureListener { onFailure(it) }
+                    }.addOnFailureListener { onFailure(it) }
+            }.addOnFailureListener { onFailure(it) }
+    }
+
+    suspend fun obtenerUsuarioPorId(idUsuario: String): Usuario? {
+        return try {
+            val document = db
+                .collection("usuarios")
+                .document(idUsuario)
+                .get()
+                .await()
+            if (document.exists()) {
+                val usuario = document.toObject(Usuario::class.java)
+
+                usuario
+            } else {
+                null
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    fun borrarUsuario(
+        idUsuario : String,
+        fotoPerfil : String,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        val usuarioRef =
+            db.collection("usuarios").document(idUsuario)
+        val imgRef = storage.getReferenceFromUrl(fotoPerfil)
+
+        usuarioRef.delete()
+            .addOnSuccessListener {
+                imgRef.delete()
+                    .addOnSuccessListener { onSuccess() }
+                    .addOnFailureListener { onFailure(it) }
+            }.addOnFailureListener { onFailure(it) }
+    }
+}
